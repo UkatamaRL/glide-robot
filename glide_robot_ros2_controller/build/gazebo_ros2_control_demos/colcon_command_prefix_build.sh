7015285acd5e351d6a6778af2719b1561c6cebaf
UkatamaRL <ukatamarl@gmail.com>
2021-11-26 15:37:41 +0900

    first commit

---
+++ glide_robot_ros2_controller/build/gazebo_ros2_control_demos/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/nagase/ROS/ros2_controller_gazebo/glide_robot_ros2_controller/install/gazebo_ros2_control/share/gazebo_ros2_control/package.sh"
---
+++ glide_robot_without_ros2_contoller/build/gazebo_ros2_control_demos/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/nagase/ROS/ros2_controller_gazebo/glide_robot_without_ros2_contoller/install/gazebo_ros2_control/share/gazebo_ros2_control/package.sh"
